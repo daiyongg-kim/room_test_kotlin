c8fabb0f51ce55605d0b9a53551764023b76945e
Daiyong Kim <daiyongg.kim@gmail.com>
2023-05-28 15:03:01 -0700

    add livedata

--- app/src/main/java/com/example/room_test_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/room_test_kotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,17 +20,16 @@ class MainActivity : AppCompatActivity() {
         ).allowMainThreadQueries()
             .build()
 
-        result_text.text = db.todoDao().getAll().toString()
+        db.todoDao().getAll().observe(this, Observer { todos ->
+            result_text.text = todos.toString()
+        })
 
         add_button.setOnClickListener{
             db.todoDao().insert(Todo(todo_edit.text.toString()))
-            result_text.text = db.todoDao().getAll().toString()
         }
 
         remove_button.setOnClickListener {
             db.todoDao().deleteAll()
-            result_text.text = db.todoDao().getAll().toString()
         }
-
     }
 }
